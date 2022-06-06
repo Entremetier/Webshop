62635c90306664b535955ac0ec7c97a4a30e45bc
Entremetier <marco.happe@icloud.com>
2022-06-06 11:49:38 +0200

    add agb.cshtml, change email in impressum.cshtml, add links in sharedLayout.cshtml, add Agb method to homeController.cs

--- Webshop/Controllers/HomeController.cs
+++ Webshop/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace Webshop.Controllers
             return View();
         }
 
+        public IActionResult Agb()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
