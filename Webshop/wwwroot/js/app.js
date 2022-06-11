23d20ad67f957f416757b27a095379f458557189
Entremetier <marco.happe@icloud.com>
2022-06-11 21:19:13 +0200

    add app.js, start password validation

---
+++ Webshop/wwwroot/js/app.js
@@ -0,0 +1,15 @@
+﻿const registerBtn = document.getElementById("registerBtn");
+const passwordOne = document.getElementById("passwordOne");
+const passwordTwo = document.getElementById("passwordTwo");
+const passwordMessageOne = document.getElementById("passwordMessageOne");
+const passwordMessageTwo = document.getElementById("passwordMessageTwo");
+
+registerBtn.addEventListener("click", validatePassword);
+
+function validatePassword() {
+    if (passwordOne.value !== passwordTwo.value)
+    {
+        passwordMessageOne.textContent = "Passwörter stimmen nicht überein";
+        passwordMessageTwo.textContent = "Passwörter stimmen nicht überein";
+    }
+}
