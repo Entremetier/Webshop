ee32b552e9102d31b60ff3046dcd1a18f86634c3
Entremetier <marco.happe@icloud.com>
2022-06-11 20:29:21 +0200

    add validation to CustomerController, update Cuatomer/Create

--- Webshop/Models/Customer.cs
+++ Webshop/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -13,14 +14,32 @@ namespace Webshop.Models
         }
 
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Anrede angeben")]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "Vornamen angeben")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Nachnamen angeben")]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "E-Mail angeben")]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Straße und Hausnummer angeben")]
         public string Street { get; set; }
+
+        [Required(ErrorMessage = "Postleizahl angeben")]
+        [RegularExpression("^\\d{4}$", ErrorMessage = "Bitte eine gültige österreichische Postleitzahl angeben")]
         public int Zip { get; set; }
+
+        [Required(ErrorMessage = "Stadt angeben")]
         public string City { get; set; }
+
         public string PwHash { get; set; }
+
         public string Salt { get; set; }
 
         public virtual ICollection<Order> Orders { get; set; }
