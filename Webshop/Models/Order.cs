791554c1617f0e769d5c9d6d90de9f1d0ae94284
Entremetier <marco.happe@icloud.com>
2022-06-22 15:22:55 +0200

    insert order and orderLine is working now

--- Webshop/Models/Order.cs
+++ Webshop/Models/Order.cs
@@ -15,7 +15,7 @@ namespace Webshop.Models
         public int Id { get; set; }
         public int CustomerId { get; set; }
         public decimal PriceTotal { get; set; }
-        public DateTime DateOrdered { get; set; }
+        public DateTime? DateOrdered { get; set; }
         public string Street { get; set; }
         public string Zip { get; set; }
         public string City { get; set; }
