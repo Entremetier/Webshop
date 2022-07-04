e829b3724a27a02028c9ed6735810110613f23d4
Entremetier <marco.happe@icloud.com>
2022-07-04 14:24:36 +0200

    minor changes in checkout and add rotativa

--- Webshop/Startup.cs
+++ Webshop/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Rotativa.AspNetCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
